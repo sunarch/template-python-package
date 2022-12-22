ff267f624ed788127336fd1138d60deea48f4c2c
András Németh <sunarch@protonmail.com>
2022-12-22 23:32:41 +0100

    Add back missing version module

--- src/projectname/version.py
+++ src/projectname/version.py
@@ -5,6 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-program_name = 'projectname'
+program_name = 'project_name'
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
